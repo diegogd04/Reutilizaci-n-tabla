fc470c7782de81a83475928ceb7528d52ee421b2
diegogd04 <diego2612004@gmail.com>
2023-12-12 19:39:42 +0100

    Reutilización tabla

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'reuttablas';
+  title = 'componenteTabla'
+  tablas = [
+    {
+      miTitulo: "LIBROS",
+      misCabeceras: ["Titulo", "Autor"],
+      misDatos: [
+        { "Titulo": "El quijote", "Autor": "Cervantes" },
+        { "Titulo": "100 años de soledad", "Autor": "Gabriel García Márquez" },
+        { "Titulo": "Los pilares de la Tierra", "Autor": "Jen Follet" }
+      ]
+    },
+    {
+      miTitulo: "REVISTAS",
+      misCabeceras: ["Titulo", "Autor"],
+      misDatos: [
+        { "Titulo": "Jara y sedal", "Autor": "A.Lopez" },
+        { "Titulo": "Pc Computer", "Autor": "MA Editorial" },
+        { "Titulo": "Muy Interesante", "Autor": "SA Edition" }
+      ]
+    },
+    {
+      miTitulo: "MÚSICA",
+      misCabeceras: ["Titulo", "Autor"],
+      misDatos: [
+        { "Titulo": "Repsody", "Autor": "Queen" },
+        { "Titulo": "Motomamy", "Autor": "Rosalía" },
+        { "Titulo": "Despechada", "Autor": "Shakira" }
+      ],
+    },
+    {
+      miTitulo: "CINE",
+      misCabeceras: ["Titulo", "Autor"],
+      misDatos: [
+        { "Titulo": "Napoleón", "Autor": "Ridney Scott" },
+        { "Titulo": "Frozen", "Autor": "Jennifer Lee" },
+        { "Titulo": "Titanic", "Autor": "James Cameron" }
+      ]
+    }
+  ]
 }
